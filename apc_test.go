87bda393c93948f2eb665b17c7c771266fd4af10
L11R <savely@krasovsky.me>
2020-05-19 13:07:29 +0300

    Initial commit; Working prototype 1

---
+++ apc_test.go
@@ -0,0 +1,23 @@
+package apc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewClient(t *testing.T) {
+	c, err := NewClient("localhost:22700", DevelopmentLogger())
+	require.NoError(t, err)
+
+	go c.Start()
+
+	assert.NoError(t, c.Logon("movchan", "12345"))
+
+	jobs, err := c.ListJobs(JobTypeAll)
+	assert.NoError(t, err)
+	_ = jobs
+
+	assert.NoError(t, c.Logoff())
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module gitlab.sovcombank.group/scb-mobile/lib/go-apc.git
+
+go 1.13
+
+require (
+	github.com/stretchr/testify v1.5.1
+	go.uber.org/zap v1.15.0
+)
