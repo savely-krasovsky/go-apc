dcebf203806007c204c601bef68034d513f0c90f
L11R <savely@krasovsky.me>
2020-05-28 02:47:06 +0300

    major refactoring

--- apc_test.go
+++ apc_test.go
@@ -22,7 +22,15 @@ func TestMain(m *testing.M) {
 }
 
 func TestClient_Logon(t *testing.T) {
-	assert.NoError(t, client.Logon("movchan", "12345"))
+	assert.NoError(t, client.Logon("GerasimE", "12345"))
+}
+
+func TestClient_ReserveHeadset(t *testing.T) {
+	assert.NoError(t, client.ReserveHeadset(71656))
+}
+
+func TestClient_ConnHeadset(t *testing.T) {
+	assert.NoError(t, client.ConnHeadset())
 }
 
 func TestClient_ListJobs(t *testing.T) {
@@ -30,6 +38,72 @@ func TestClient_ListJobs(t *testing.T) {
 	require.NoError(t, err)
 
 	_ = jobs
+}
+
+func TestClient_AttachJob(t *testing.T) {
+	assert.NoError(t, client.AttachJob("TEST_DIT"))
+}
+
+/*
+func TestClient_ListCallLists(t *testing.T) {
+	lists, err := client.ListCallLists()
+	require.NoError(t, err)
+
+	_ = lists
+}
+
+func TestClient_AGTListCallFields(t *testing.T) {
+	fields, err := client.ListCallFields("list1")
+	require.NoError(t, err)
+
+	_ = fields
+}
+
+func TestClient_ListDataFields(t *testing.T) {
+	fields, err := client.ListDataFields(ListTypeOutbound)
+	require.NoError(t, err)
+
+	_ = fields
+}
+
+func TestClient_SetNotifyKeyField(t *testing.T) {
+	assert.NoError(t, client.SetNotifyKeyField(ListTypeOutbound, "DEBT_ID"))
+}
+
+func TestClient_SetDataField(t *testing.T) {
+	assert.NoError(t, client.SetDataField(ListTypeOutbound, "FIO"))
+	assert.NoError(t, client.SetDataField(ListTypeOutbound, "TSUMPAY"))
+	assert.NoError(t, client.SetDataField(ListTypeOutbound, "PHONE_ID1"))
+}
+
+func TestClient_AvailWork(t *testing.T) {
+	assert.NoError(t, client.AvailWork())
+}
+
+func TestClient_ReadyNextItem(t *testing.T) {
+	assert.NoError(t, client.ReadyNextItem())
+
+	time.Sleep(10 * time.Minute)
+}
+*/
+
+func TestClient_ListKeys(t *testing.T) {
+	keys, err := client.ListKeys()
+	assert.NoError(t, err)
+
+	_ = keys
+}
+
+func TestClient_DetachJob(t *testing.T) {
+	assert.NoError(t, client.DetachJob())
+}
+
+func TestClient_DisconnHeadset(t *testing.T) {
+	assert.NoError(t, client.DisconnHeadset())
+}
+
+func TestClient_FreeHeadset(t *testing.T) {
+	assert.NoError(t, client.FreeHeadset())
 }
 
 func TestClient_Logoff(t *testing.T) {
--- proto.go
+++ proto.go
@@ -10,11 +10,11 @@ import (
 
 const (
 	// Message separator
-	RS byte = 0x1E
+	RS rune = 0x1E
 	// Incomplete message separator
-	ETB byte = 0x17
+	ETB rune = 0x17
 	// End of text
-	ETX byte = 0x03
+	ETX rune = 0x03
 )
 
 type EventType byte
@@ -28,6 +28,39 @@ const (
 	EventTypeNotification EventType = 'N'
 )
 
+type MessageCode string
+
+const (
+	MessageCodeComplete    MessageCode = "M00000"
+	MessageCodeDataMessage MessageCode = "M00001"
+	MessageCodePending     MessageCode = "S28833"
+)
+
+type MessageCodeType byte
+
+const (
+	MessageCodeTypeMessage MessageCodeType = 'M'
+	MessageCodeTypeError   MessageCodeType = 'E'
+	MessageCodeTypeStatus  MessageCodeType = 'S'
+)
+
+func (mc MessageCode) Type() MessageCodeType {
+	if len(mc) < 1 {
+		return 0
+	}
+
+	switch {
+	case mc[0] == byte(MessageCodeTypeMessage):
+		return MessageCodeTypeMessage
+	case mc[0] == byte(MessageCodeTypeError):
+		return MessageCodeTypeError
+	case mc[0] == byte(MessageCodeTypeStatus):
+		return MessageCodeTypeStatus
+	}
+
+	return 0
+}
+
 type Event struct {
 	Keyword   string
 	Type      EventType
@@ -35,6 +68,11 @@ type Event struct {
 	ProcessID uint32
 	InvokeID  uint32
 	Segments  []string
+
+	CommandStatus uint8
+	MessageCode   MessageCode
+
+	Incomplete bool
 }
 
 func encodeCommand(keyword string, invokeID uint32, args ...string) ([]byte, error) {
@@ -67,56 +105,89 @@ func encodeCommand(keyword string, invokeID uint32, args ...string) ([]byte, err
 	buf.WriteString(fmt.Sprintf("%-4d", len(args)))
 
 	if len(args) > 0 {
-		buf.WriteByte(RS)
+		buf.WriteByte(byte(RS))
 		for i, arg := range args {
 			buf.WriteString(arg)
 
 			if len(args)-1 != i {
-				buf.WriteByte(RS)
+				buf.WriteByte(byte(RS))
 			}
 		}
 	}
-	buf.WriteByte(ETX)
+	buf.WriteByte(byte(ETX))
 
 	return buf.Bytes(), nil
 }
 
-func decodeEvent(b []byte) (event *Event, err error) {
-	if len(b) < 56 {
-		return nil, errors.New("event len should be less or equal to 55 bytes")
+func decodeEvent(atom string) (event Event, err error) {
+	if len(atom) < 56 {
+		return Event{}, errors.New("event len should be less or equal to 55 bytes")
 	}
 
-	event = &Event{
-		Keyword: strings.TrimSpace(string(b[:20])),
-		Type:    EventType(b[20]),
-		Client:  strings.TrimSpace(string(b[21:41])),
+	event = Event{
+		Keyword: strings.TrimSpace(atom[:20]),
+		Type:    EventType(atom[20]),
+		Client:  strings.TrimSpace(atom[21:41]),
 	}
 
-	processID, err := strconv.Atoi(strings.TrimSpace(string(b[41:47])))
+	processID, err := strconv.Atoi(strings.TrimSpace(atom[41:47]))
 	if err != nil {
-		return nil, err
+		return Event{}, err
 	}
 	event.ProcessID = uint32(processID)
 
-	invokeID, err := strconv.Atoi(strings.TrimSpace(string(b[47:51])))
+	invokeID, err := strconv.Atoi(strings.TrimSpace(atom[47:51]))
 	if err != nil {
-		return nil, err
+		return Event{}, err
 	}
 	event.InvokeID = uint32(invokeID)
 
-	numberOfSegments, err := strconv.Atoi(strings.TrimSpace(string(b[51:55])))
+	numberOfSegments, err := strconv.Atoi(strings.TrimSpace(atom[51:55]))
 	if err != nil {
-		return nil, err
+		return Event{}, err
 	}
 
-	if numberOfSegments > 0 && len(b) > 56 {
-		segments := bytes.Split(b[56:], []byte{RS})
+	if numberOfSegments > 0 && len(atom) > 56 {
+		segments := strings.Split(atom[56:], string(RS))
 		for i, s := range segments {
-			if len(segments)-1 == i {
-				event.Segments = append(event.Segments, string(bytes.TrimSuffix(s, []byte{ETX})))
-				break
+			// Trim last byte if it reached the end
+			if i == len(segments)-1 {
+				if strings.HasSuffix(s, string(ETB)) {
+					event.Incomplete = true
+					s = strings.TrimSuffix(s, string(ETB))
+				}
+
+				if strings.HasSuffix(s, string(ETX)) {
+					s = strings.TrimSuffix(s, string(ETX))
+				}
 			}
-			event.Segments = append(event.Segments, string(s))
+
+			// Read command status and message code
+			switch i {
+			case 0:
+				t, err := strconv.Atoi(s)
+				if err != nil {
+					return Event{}, err
+				}
+				event.CommandStatus = uint8(t)
+				continue
+			case 1:
+				event.MessageCode = MessageCode(s)
+				continue
+			}
+
+			// Workaround: strip continue messages
+			if j := strings.Index(s, string(ETB)); j != -1 {
+				s = s[:j]
+			}
+
+			// Some responses may have windows-1251 encoded strings, so do some decoding
+			/*s, err := charmap.Windows1251.NewDecoder().Bytes(s)
+			if err != nil {
+				return Event{}, err
+			}*/
+
+			event.Segments = append(event.Segments, s)
 		}
 	}
 
