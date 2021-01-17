1f9c77c834305506b2eca6cfa48348b288c468dc
L11R <savely@krasovsky.me>
2021-01-17 16:59:30 +0300

    Custom decoder support and more docs

--- apc.go
+++ apc.go
@@ -11,17 +11,17 @@ import (
 	"time"
 
 	tls "github.com/L11R/apc-tls"
-
 	"gitlab.sovcombank.group/scb-mobile/lib/go-apc.git/pool"
 	"go.uber.org/atomic"
 	"go.uber.org/zap"
-	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/encoding"
 )
 
 type Options struct {
 	Timeout    *time.Duration
 	LogLevel   LogLevel
 	LogHandler LogHandler
+	Decoder    *encoding.Decoder
 }
 
 type Option func(*Options)
@@ -66,8 +66,18 @@ func WithLogHandler(logLevel LogLevel, handler LogHandler) Option {
 	}
 }
 
+// WithDecoder returns an Option with custom decoder
+// e.g w/ charmap.Windows1251.NewDecoder().
+func WithDecoder(decoder *encoding.Decoder) Option {
+	return func(options *Options) {
+		options.Decoder = decoder
+	}
+}
+
 const (
+	// ConnOK means that connection is currently online
 	ConnOK uint32 = iota
+	// ConnClosed means that connection is currently closing or already closed
 	ConnClosed
 )
 
@@ -76,9 +86,12 @@ var (
 	ErrHelloNotReceived = errors.New("hello not received")
 )
 
+// request is the private struct that represents a request to an APC server
 type request struct {
-	context   context.Context
-	cancel    context.CancelFunc
+	// context and cancel func to control a cancellation process
+	context context.Context
+	cancel  context.CancelFunc
+	// each request has own event channel w/ a bunch of possible responses
 	eventChan chan Event
 }
 
@@ -86,16 +99,29 @@ type Client struct {
 	opts   *Options
 	logger *logger
 
+	// Stores a current state of an underlying connection, e.g. ConnOK or ConnClosed
 	state *atomic.Uint32
 
-	conn          net.Conn
-	events        chan Event
+	// underlying connection
+	conn net.Conn
+	// decoder to deal with old encodings like Windows-1251
+	decoder io.Reader
+	// channel w/ decoded events that were received from a connection
+	events chan Event
+	// dedicated channel for notification events only
 	notifications chan Notification
-	shutdown      chan error
+	// channel to shut down the *Client when the time will come
+	shutdown chan error
 
+	// a pool of invoke ids that are used by requests map
+	//
+	// Each method execution requires own invoke ID; for example a user of this library wants to execute
+	// two methods at the same time, then this pool will give two invoke IDs: 1 and 2;
+	// after execution they will be released for further use.
 	invokeIDPool *pool.InvokeIDPool
-	requests     map[uint32]*request
-
+	// a map that contains a set of currently executing requests
+	requests map[uint32]*request
+	// a mutex to control an access to requests map
 	mu sync.RWMutex
 }
 
@@ -109,11 +135,14 @@ func NewClient(addr string, opts ...Option) (*Client, error) {
 		opt(options)
 	}
 
+	// Initiate the TCP connection to an APC server
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		return nil, fmt.Errorf("error while dialing: %w", err)
 	}
 
+	// Use patched tls package (w/ disabled BEAST attack mitigation) to wrap the TCP connection;
+	// Otherwise old APC server has random disconnects after a dozen of consistent writes.
 	tlsConn := tls.Client(conn, &tls.Config{
 		AvayaCompatibility: true,
 		InsecureSkipVerify: true,
@@ -132,15 +161,21 @@ func NewClient(addr string, opts ...Option) (*Client, error) {
 	if options.LogHandler != nil {
 		c.logger = newLogger(options.LogLevel, options.LogHandler)
 	}
+	if options.Decoder != nil {
+		c.decoder = tlsConn
+	} else {
+		c.decoder = options.Decoder.Reader(tlsConn)
+	}
 
+	// Goroutine that starts event reading from the connection
 	go func() {
 		c.shutdown <- c.readEvents()
 	}()
 
-	// Read AGTSTART
+	// Read the first AGTSTART event before returning the *Client
 	event := <-c.events
 
-	// Check that first notification message is correct
+	// Check that the first notification message is correct
 	if event.Keyword != "AGTSTART" ||
 		!event.IsStart() {
 		c.logger.log(newLogEntry(LogLevelError, "Server cannot accept new clients!"))
@@ -153,16 +188,20 @@ func NewClient(addr string, opts ...Option) (*Client, error) {
 // Start starts main event loop handler.
 func (c *Client) Start() error {
 	for {
+		// Wait for events, error or an execution of Stop()
 		select {
 		case event := <-c.events:
+			// Assign notification events own invoke IDs to get them processed
 			if event.Type == EventTypeNotification {
 				event.InvokeID = math.MaxUint32
 			}
 
+			// Look up for a request
 			c.mu.RLock()
 			r, ok := c.requests[event.InvokeID]
 			c.mu.RUnlock()
 
+			// In case of success, send received event into own request event channel
 			if ok {
 				r.eventChan <- event
 			}
@@ -201,8 +240,11 @@ func (c *Client) Start() error {
 func (c *Client) Notifications(ctx context.Context) <-chan Notification {
 	c.notifications = make(chan Notification, 128)
 
+	// Notifications has own request...
 	r := newRequest(ctx)
 
+	// ...inside request map, but it has fake invoke ID to avoid conflicts with real ones.
+	// Real invoke IDs are limited to 4 digits (9999), while MaxUint32 is 4294967295.
 	c.mu.Lock()
 	c.requests[math.MaxUint32] = r
 	c.mu.Unlock()
@@ -222,9 +264,6 @@ func (c *Client) Notifications(ctx context.Context) <-chan Notification {
 }
 
 func (c *Client) readEvents() error {
-	// Server still uses Windows1251 as default encoding.
-	decoder := charmap.Windows1251.NewDecoder().Reader(c.conn)
-
 	// Main event loop.
 	for {
 		// Set actual
@@ -235,10 +274,13 @@ func (c *Client) readEvents() error {
 			}
 		}
 
-		// 4096 bytes is the maximum request size, but 256 should be enough
+		// 4096 bytes is the maximum request size, but 256 should be enough;
+		// could be increased in case of getting errors.
 		buf := make([]byte, 256)
 
-		n, err := decoder.Read(buf)
+		// Without decoder it will use c.tlsConn directly; read through decoder to avoid encoding problems
+		// (to activate it use WithDecoder()); for example in Russia APC server uses Windows-1251.
+		n, err := c.decoder.Read(buf)
 		if err != nil {
 			if err == io.EOF {
 				c.logger.log(newLogEntry(LogLevelInfo, "EOF received.", map[string]interface{}{"error": err}))
